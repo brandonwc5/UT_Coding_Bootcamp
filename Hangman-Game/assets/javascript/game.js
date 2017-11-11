2d772922d2e8a7d76c70ef7eb48c0e74db70edc4
brandonwc5 <brandonwc5@yahoo.com>
2017-11-11 11:50:48 -0600

    Resetting but its a hard reset, so cant keep winCount/LossCount

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,4 +1,4 @@
-alert("Let's begin!");
+alert("The category is Presidents");
 
 //set up my variables
 var word = ["WASHINGTON", "ADAMS", "LINCOLN", "OBAMA", "BUSH", "KENNEDY", "POLK", "CARTER", "BUCHANNAN", "MADISON", "JACKSON"];
@@ -8,9 +8,10 @@ var guessesLeft= 7;
 var correctGuessArray= [];
 var incorrectGuessArray= [];
 var blank = "__&nbsp;";
+var winCount = 0;
 
 //add event listener
-document.addEventListener("keypress", getEvt);
+document.addEventListener("keypress", game);
 
 var answer = word[currWord];
 
@@ -21,11 +22,7 @@ for(i=0; i<answer.length;i++){
 	correctGuessArray.push(blank);
 }
 
-function endGame(){
-
-}
-
-function getEvt(e){
+function game(e){
 	//make user guess uppercase and don't let them use non-letters
 	var userLetter = e.key.toUpperCase();
 	userLetter = userLetter.replace(/[^A-Za-z]+$/, '');
@@ -39,19 +36,24 @@ function getEvt(e){
 			}
 		}
 	} else {
-		incorrectGuessArray.push(userLetter); 
-		document.getElementById("wrong").innerHTML = incorrectGuessArray.join("");
-		guessesLeft--;		
-		function what(){
-			document.getElementById("guesses").innerHTML = guessesLeft;
-			};
-		what();
+		//Push incorrect letter to corresponding array only if it is not in that array already
+		while(incorrectGuessArray.indexOf(userLetter)<0) {
+			incorrectGuessArray.push(userLetter); 
+			document.getElementById("wrong").innerHTML = incorrectGuessArray.join("");
+			guessesLeft--;
+			function what(){
+				document.getElementById("guesses").innerHTML = guessesLeft;
+				};
+			what();
+		}
 	}
 	if(correctGuessArray.join("") == answer){
 		alert("You win!");
-
+		document.location.reload();
  	}
+
 	if(incorrectGuessArray.length == 7){
  		alert("You Lose!");
+ 		document.location.reload();
  	}
  }
